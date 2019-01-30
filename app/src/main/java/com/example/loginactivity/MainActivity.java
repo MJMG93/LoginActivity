9de1967589c17c4e0ec04b9e3a67009ed386227b
michaelmogannam <mjmg93@humboldt.edu>
2019-01-30 14:45:54 -0800

    Initial commit

---
+++ app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.loginactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText) findViewById(R.id.etName);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Info = (TextView) findViewById(R.id.tvInfo);
+        Login = (Button) findViewById(R.id.btnLogin);
+
+
+    }
+
+    private void validate(String userName, String userPassword) {
+        if((userName == "Admin") && (userPassword == "1234")) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+    }
+
+}
